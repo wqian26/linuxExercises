0da5bf3e8a1ad15f11a53b32a035651f9bf60983
Cynthia Xu <xu568@wisc.edu>
2024-03-05 12:19:21 -0600

    finished it

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+mkdir five
+for i in {1..5}; do
+    mkdir five/dir$i
+    for j in {1..4}; do
+	> five/dir$i/file$j
+	for k in $(seq 1 $((j))); do
+            echo $((j)) >> five/dir$i/file$j
+        done
+    done
+done
